03772a91356ea53fa0abc1ca43322da92cae5c81
Ryan Silan <ryan.silan@gmail.com>
2017-08-01 19:14:20 -0400

    Added variables. Added get methods.

--- ComputerLocator2/commandexecutor/ObtainComputerInformation.cs
+++ ComputerLocator2/commandexecutor/ObtainComputerInformation.cs
@@ -10,14 +10,21 @@ namespace ComputerLocator2.commandexecutor
     {
         string cmd = "/c %windir%\\System32\\wbem\\WMIC.exe /node:";
         string serialNumberCommand = " bios get serialnumber";
+        string modelNumberCommand = " computersystem get model";
+        string nameCommand = " computersystem get name";
+        string name = null;
+        string ipAddress = null;
+        string model = null;
+        string serialNumber = null; 
 
-        public void getComputerInfo(string name)
+        public void getComputerInfo(string ipAddress)
         {
-            
+            this.ipAddress = ipAddress;
+            System.Diagnostics.Debug.WriteLine(getSerialNumber(ipAddress)); 
 
         }
 
-        private string getSerialNumber(string name)
+        private string getSerialNumber(string ipAddress)
         {
             var commandExecutor = new CommandExecutor();
             string command = cmd + name + serialNumberCommand;
